4410f050b644b39153c39c725e693101ddf424b9
poliweb <web@poliweb.su>
2024-12-24 18:04:18 +0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="container px-5 py-24 mx-auto">
-        <h1 class="text-red-500">PoliWeb</h1>
+        <h1 class="text-red-500">PoliWeb - {{ PoliWeb[0].name }}</h1>
     </div>
 </template>
 
 <script setup>
-
+const PoliWeb = [
+    {
+        name: "Павел Полистовский",
+    }
+]
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
